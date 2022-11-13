c2ac9ef87494614e769a5ef3a12b5ac5bd62fc32
ihebsaadaoui <iheb.saadaoui@esprit.tn>
2022-11-13 01:02:10 +0100

    update test

--- src/test/java/tn/esprit/rh/achat/util/produit.java
+++ src/test/java/tn/esprit/rh/achat/util/produit.java
@@ -1,4 +1,4 @@
-package tn.esprit.rh.achat;
+package tn.esprit.rh.achat.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
